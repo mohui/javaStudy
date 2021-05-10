ecab9f514241bd1e67f4f3876ba5df3bce3f8850
王贺辉 <wanghehui@bjknrt.com>
2021-05-10 17:23:52 +0800

    引入全局样式

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import './style/main.css';
 
-import ElementUI from 'element-ui'
+import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
 
